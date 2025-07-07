81ea9d1353f2d46b431135b776dd7b925300d16f
凡星 <m13631897256@163.com>
2025-07-07 17:32:13 +0800

    useCallback 例子

--- hooks-demo/src/App.jsx
+++ hooks-demo/src/App.jsx
@@ -1,42 +1,38 @@
-import { useEffect, useState, useMemo } from 'react'
+import { useEffect, useState, useCallback } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function Child({ data }) {
+function Child({ callback }) {
   useEffect(() => {
-    console.log('查询条件：', data);
-  }, [data]);
+    callback();
+  }, [callback]);
 
   return <div>子组件</div>
 };
 
 function App() {
-  // useMemo 例子
+  // useCallback 例子
   const [name, setName] = useState('');
   const [phone, setPhone] = useState('');
   const [kw, setKw] = useState('');
 
-  // 这种方式，有个问题：子组件并没有监听 kw 的变化，但是结果却是子组件也被触发渲染了
-  // 原因大概是 data 数据发生改变，导致子组件重新渲染
-  // const data = {
-  //   name,
-  //   phone
+  // 问题：当我们修改任何状态值，都会触发子组件的回调函数执行，但是 callback 没有作任何变化。
+  // const callback = () => {
+  //   console.log('我是 callback');
   // };
 
-  // 它相当于把父组件需要传递的参数做了一个标记，无论父组件其他状态更新任何值，都不会影响要传递给子组件的对象。
-  // Q：还是不太理解
-  const data = useMemo(() => ({
-    name,
-    phone
-  }), [name, phone]);
+  // 无论修改其他任何属性，都不会触发子组件的副作用
+  const callback = useCallback(() => {
+    console.log('我是 callback');
+  }, []);
 
   return (
     <div className='App'>
       <input type="text" onChange={(e) => setName(e.target.value)} placeholder='请输入姓名' />
       <input type="text" onChange={(e) => setPhone(e.target.value)} placeholder='请输入电话' />
       <input type="text" onChange={(e) => setKw(e.target.value)} placeholder='请输入姓名关键词' />
-      <Child data={data} />
+      <Child callback={callback} />
     </div>
   );
 }
