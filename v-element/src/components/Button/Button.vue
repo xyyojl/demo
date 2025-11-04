555a36602c936ffe040a0dff80a0bfad62a45bfe
凡星 <m13631897256@163.com>
2025-11-04 11:20:39 +0800

    完成 Button 组件的基础编码部分

---
+++ v-element/src/components/Button/Button.vue
@@ -0,0 +1,42 @@
+<template>
+    <button
+        ref="_ref"
+        class="vk-button"
+        :class="{
+            [`vk-button--${type}`]: type,
+            [`vk-button--${size}`]: size,
+            'is-plain': plain,
+            'is-round': round,
+            'is-circle': circle,
+            'is-disabled': disabled,
+        }"
+        :disabled="disabled"
+        :type="nativeType"
+        :autofocus="autofocus"
+    >
+        <span>
+            <slot />
+        </span>
+    </button>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+import type { ButtonProps } from './types';
+defineOptions({
+    name: 'VkButton'
+});
+withDefaults(defineProps<ButtonProps>(), {
+    nativeType: 'button'
+});
+
+const _ref = ref<HTMLButtonElement>();
+
+defineExpose({
+    ref: _ref
+})
+</script>
+
+<style scoped>
+
+</style>
