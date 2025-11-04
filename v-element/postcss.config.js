51957a72ae92060f50f1c83952d6514e1065e717
凡星 <m13631897256@163.com>
2025-11-04 23:27:09 +0800

    使用 PostCSS 生成颜色变量&修改 postcss.config.js 修改成数组式写法支持嵌套插件配置

--- v-element/postcss.config.js
+++ v-element/postcss.config.js
@@ -1,6 +1,20 @@
+import postcssEach from 'postcss-each';
+import postcssEachVariables from 'postcss-each-variables';
+import postcssNested from 'postcss-nested';
+import postcssFor from 'postcss-for';
+import postcssColorMix from 'postcss-color-mix';
+
 export default {
-  plugins: {
-    'postcss-nested': {},
-    'postcss-each': {},
-  },
+  plugins: [
+    postcssEachVariables(),
+    postcssNested(),
+    postcssEach({
+      plugins: {
+        beforeEach: [
+          postcssFor(),
+          postcssColorMix(),
+        ],
+      },
+    }),
+  ],
 };
