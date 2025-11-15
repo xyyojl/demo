8077f02bb9142bb324fa77a01cd532223bb4b0e4
凡星 <m13631897256@163.com>
2025-11-15 10:42:03 +0800

    Collaspe 添加Slide动画 第二部分

--- v-element/src/components/Collapse/CollapseItem.vue
+++ v-element/src/components/Collapse/CollapseItem.vue
@@ -17,8 +17,10 @@
       <slot name="title">{{ title }}</slot>
     </div>
     <Transition name="slide" v-on="transitionEvents">
-      <div class="vk-collapse-item__content" :id="`item-content-${name}`" v-show="isActive">
-        <slot />
+      <div class="vk-collapse-item__wrapper" v-show="isActive">
+        <div class="vk-collapse-item__content" :id="`item-content-${name}`">
+          <slot />
+        </div>
       </div>
     </Transition>
   </div>
@@ -41,21 +43,25 @@ const handleClick = () => {
 const transitionEvents: Record<string, (el: HTMLElement) => void> = {
   beforeEnter(el) {
     el.style.height = '0px';
+    el.style.overflow = 'hidden';
   },
   enter(el) {
     el.style.height = `${el.scrollHeight}px`;
   },
   afterEnter(el) {
     el.style.height = '';
+    el.style.overflow = '';
   },
   beforeLeave(el) {
     el.style.height = `${el.scrollHeight}px`;
+    el.style.overflow = 'hidden';
   },
   leave(el) {
     el.style.height = '0px';
   },
   afterLeave(el) {
     el.style.height = '';
+    el.style.overflow = '';
   }
 };
 </script>
