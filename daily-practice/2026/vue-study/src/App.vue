e7af006363acb7b07719d3330bb52b9d0f01c8fe
凡星 <m13631897256@163.com>
2026-01-29 10:44:42 +0800

    实现el-switch开关组件

--- daily-practice/2026/vue-study/src/App.vue
+++ daily-practice/2026/vue-study/src/App.vue
@@ -2,20 +2,26 @@
   <h2>hello vue</h2>
   <ElRate v-model="value1" /> {{ value1 }}
   <ElRate v-model="value2" :max="6" /> {{ value2 }}
+  <ElSwitch v-model="value3" /> {{ value3 ? '开' : '关' }}
+  <ElSwitch v-model="value4" /> {{ value4 ? '开' : '关' }}
 </template>
 
 <script>
 import ElRate from './components/ElRate.vue';
+import ElSwitch from './components/ElSwitch.vue';
 export default {
   name: 'App',
   data() {
     return {
       value1: 0,
-      value2: 3
+      value2: 3,
+      value3: true,
+      value4: false
     }
   },
   components: {
-    ElRate
+    ElRate,
+    ElSwitch
   }
 }
 </script>
---
+++ daily-practice/2026/vue-study/src/components/ElSwitch.vue
@@ -0,0 +1,57 @@
+<template>
+    <div :class="switchClass" @click="handleClick">
+        <div class="switch__handle"></div>
+    </div>
+</template>
+
+<script>
+/* 
+实现需求：el-switch开关组件
+- 状态：开/关
+- 交互：点击切换状态
+*/
+export default {
+  name: 'ElSwitch',
+  props: ['modelValue'],
+  emits: ['update:modelValue'],
+  computed: {
+    switchClass() {
+       return {
+        switch: true,
+        'switch--closed': !this.modelValue
+       };
+    }
+  },
+  methods: {
+    handleClick() {
+        this.$emit('update:modelValue', !this.modelValue);
+    }
+  }
+}
+</script>
+
+<style scoped>
+.switch {
+    width: 28px;
+    height: 16px;
+    border-radius: 100px;
+    background: #1890ff;
+    cursor: pointer;
+}
+.switch--closed {
+    background: rgba(0, 0, 0, 0.25);
+}
+.switch--closed .switch__handle {
+    left: 14px;
+}
+.switch__handle {
+    width: 12px;
+    height: 12px;
+    background: white;
+    border-radius: 50%;
+    position: relative;
+    left: 2px;
+    top: 2px;
+    transition: .2s;
+}
+</style>
