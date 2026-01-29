3d0109e6d358a86de8d831d78d954de33244f191
凡星 <m13631897256@163.com>
2026-01-29 16:42:45 +0800

    自定义输入框防抖指令

--- daily-practice/2026/vue-study/src/App.vue
+++ daily-practice/2026/vue-study/src/App.vue
@@ -4,6 +4,8 @@
   <ElRate v-model="value2" :max="6" /> {{ value2 }}
   <ElSwitch v-model="value3" /> {{ value3 ? '开' : '关' }}
   <ElSwitch v-model="value4" /> {{ value4 ? '开' : '关' }}
+  <h2>函数防抖指令</h2>
+  <input v-debouncing="handleChange" type="text">
 </template>
 
 <script>
@@ -22,6 +24,20 @@ export default {
   components: {
     ElRate,
     ElSwitch
+  },
+  methods: {
+    handleChange() {
+      console.log(123);
+    }
+  },
+  directives: {
+    debouncing(el, binding) {
+      let timer;
+      el.oninput = function() {
+        clearTimeout(timer);
+        timer = setTimeout(binding.value, 200);
+      }
+    }
   }
 }
 </script>
