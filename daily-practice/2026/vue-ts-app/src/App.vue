54f74c368fabea967deb1c1592129e96a6e865d0
凡星 <m13631897256@163.com>
2026-02-05 15:11:24 +0800

    脚手架安装及第三方模块安装与配置

---
+++ daily-practice/2026/vue-ts-app/src/App.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts"></script>
+
+<template>
+  <h1>You did it!</h1>
+  <p>
+    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
+    documentation
+  </p>
+</template>
+
+<style lang="scss">
+@import '@/assets/styles/reset.scss';
+@import '@/assets/styles/iconfont.scss';
+@import '@/assets/styles/common.scss';
+</style>
