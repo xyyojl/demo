58db0f48c472bb3588c0ebabe258917acd0d872e
凡星 <m13631897256@163.com>
2026-02-05 16:27:37 +0800

    对axios进行二次封装处理

---
+++ daily-practice/2026/vue-ts-app/src/utils/http.ts
@@ -0,0 +1,42 @@
+import axios from 'axios'
+import type { AxiosRequestConfig, AxiosResponse } from 'axios'
+
+const instance = axios.create({
+  baseURL: 'http://localhost:3000/',
+  timeout: 5000
+});
+
+// 添加请求拦截器
+instance.interceptors.request.use(function (config) {
+  return config;
+}, function (error) {
+  return Promise.reject(error);
+});
+
+// 添加响应拦截器
+instance.interceptors.response.use(function (response) {
+  return response;
+}, function (error) {
+  return Promise.reject(error);
+});
+
+interface Data {
+  [index: string]: unknown
+}
+
+interface Http {
+  get: (url: string, data?: Data, config?: AxiosRequestConfig) => Promise<AxiosResponse>
+  post: (url: string, data?: Data, config?: AxiosRequestConfig) => Promise<AxiosResponse>
+  put: (url: string, data?: Data, config?: AxiosRequestConfig) => Promise<AxiosResponse>
+  patch: (url: string, data?: Data, config?: AxiosRequestConfig) => Promise<AxiosResponse>
+  delete: (url: string, data?: Data, config?: AxiosRequestConfig) => Promise<AxiosResponse>
+}
+
+const http: Http = {
+  get: (url, data, config) => instance.get(url, { params: data, ...config }),
+  post: (url, data, config) => instance.post(url, data, config),
+  put: (url, data, config) => instance.put(url, data, config),
+  patch: (url, data, config) => instance.patch(url, data, config),
+  delete: (url, data, config) => instance.delete(url, { params: data, ...config }),
+}
+export default http
--- daily-practice/2026/vue-ts-app/src/views/Login/Login.vue
+++ daily-practice/2026/vue-ts-app/src/views/Login/Login.vue
@@ -5,7 +5,14 @@
 </template>
 
 <script setup lang="ts">
-
+// 测试axios二次封装
+import http from '@/utils/http'
+http.post('/users/login', {
+  email: 'huangrong@imooc.com',
+  pass: 'huangrong'
+}).then(res => {
+  console.log(res.data);
+})
 </script>
 
 <style scoped lang="scss">
