4b03a0f8f93baf2cbe0fc6596a5e95f0f0e40dfa
凡星 <m13631897256@163.com>
2026-02-06 10:18:18 +0800

    搭建登录界面及iconfont图标使用

--- daily-practice/2026/vue-ts-app/src/views/Login/Login.vue
+++ daily-practice/2026/vue-ts-app/src/views/Login/Login.vue
@@ -1,30 +1,134 @@
 <template>
-  <div>
-    hello login
+  <div class="login">
+    <div class="header">
+      <span class="header-logo">
+        <i class="iconfont icon-vue"></i>
+        <i class="iconfont icon-icon-test"></i>
+        <i class="iconfont icon-typescript"></i>
+      </span>
+      <span class="header-title">在线考勤系统</span>
+    </div>
+    <div class="desc">
+      零基础从入门到进阶，系统掌握前端三大热门技术(Vue、React、TypeScript)
+    </div>
+    <el-form ref="ruleFormRef" :model="ruleForm" :rules="rules" label-width="120px" class="main">
+      <el-form-item label="邮箱" prop="email">
+        <el-input v-model="ruleForm.email" type="text" placeholder="请输入邮箱" />
+      </el-form-item>
+      <el-form-item label="密码" prop="pass">
+        <el-input v-model="ruleForm.pass" type="password" placeholder="请输入密码" />
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" @click="submitForm(ruleFormRef)" auto-insert-space>
+          登录
+        </el-button>
+      </el-form-item>
+    </el-form>
+    <div class="users">
+      <el-row :gutter="20">
+        <el-col :span="12">
+          <h3>测试账号，<el-button>一键登录</el-button></h3>
+          <p>邮箱：huangrong@imooc.com</p>
+          <p>密码：huangrong</p>
+        </el-col>
+        <el-col :span="12">
+          <h3>测试账号，<el-button>一键登录</el-button></h3>
+          <p>邮箱：hongqigong@imooc.com</p>
+          <p>密码：hongqigong</p>
+        </el-col>
+      </el-row>
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
-/* // 测试axios二次封装
-import http from '@/utils/http'
-http.post('/users/login', {
-  email: 'huangrong@imooc.com',
-  pass: 'huangrong'
-}).then(res => {
-  console.log(res.data);
-}) */
+import { reactive, ref } from 'vue'
 
-import { useUsersStore } from '@/stores/users';
-const usersStore = useUsersStore();
+import type { FormInstance, FormRules } from 'element-plus'
 
-usersStore.loginAction({
-  email: 'huangrong@imooc.com',
-  pass: 'huangrong'
-}).then(res => {
-  console.log(res.data);
+interface User {
+  email: string
+  pass: string
+}
+
+const ruleFormRef = ref<FormInstance>()
+const ruleForm = reactive<User>({
+  email: '',
+  pass: ''
 })
+const rules = reactive<FormRules<User>>({
+})
+
+const submitForm = (formEl: FormInstance | undefined) => {
+  if (!formEl) return
+  formEl.validate((valid) => {
+    if (valid) {
+      console.log('submit!')
+    } else {
+      console.log('error submit!')
+    }
+  })
+}
 </script>
 
 <style scoped lang="scss">
-
+.login {
+  width: 100vw;
+  height: 100vh;
+  background: url('@/assets/images/login-bg.svg') no-repeat center 110px;
+  background-size: 100%;
+  .header {
+    height: 44px;
+    line-height: 44px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-size: 34px;
+    padding-top: 100px;
+  }
+  .header-logo {
+    .icon-vue,
+    .icon-icon-test,
+    .icon-typescript {
+      margin-right: 5px;
+      font-size: inherit;
+    }
+    .icon-vue {
+      color: green;
+    }
+    .icon-icon-test {
+      color: #deb887;
+    }
+    .icon-typescript {
+      color: blue;
+    }
+  }
+  .header-title {
+    margin-left: 30px;
+    font-weight: 700;
+    font-size: 30px;
+  }
+  .desc {
+    text-align: center;
+    padding-top: 30px;
+    color: rgba(0, 0, 0, 0.45);
+    font-size: 16px;
+  }
+  .main {
+    width: 500px;
+    margin: 0 auto;
+    padding-top: 50px;
+  }
+  .users {
+    width: 500px;
+    margin: 60px auto;
+    color: rgba(0,0,0,.65);
+    h3 {
+      font-size: 16px;
+    }
+    p {
+      margin: 20px;
+    }
+  }
+}
 </style>
