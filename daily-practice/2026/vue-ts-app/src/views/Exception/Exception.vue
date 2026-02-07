ca757bbbc85e1cb22780f4f0b5c0e8187b32a69d
凡星 <m13631897256@163.com>
2026-02-07 10:55:58 +0800

    异常考勤页时间线布局

--- daily-practice/2026/vue-ts-app/src/views/Exception/Exception.vue
+++ daily-practice/2026/vue-ts-app/src/views/Exception/Exception.vue
@@ -1,13 +1,81 @@
 <template>
-  <div>
-    hello exception
+  <div class="exception-title">
+    <el-button type="primary">异常处理</el-button>
+    <el-space>
+      <el-button plain>{{ year }}年</el-button>
+      <el-select v-model="month">
+        <el-option v-for="item in 12" :key="item" :value="item" :label="item + '月'" />
+      </el-select>
+    </el-space>
   </div>
+  <el-row :gutter="20">
+    <el-col :span="12">
+      <el-empty v-if="false" description="暂无异常考勤" />
+      <el-timeline v-else>
+        <el-timeline-item timestamp="2018/4/12" placement="top">
+          <el-card>
+            <el-space>
+              <h4>旷工</h4>
+              <p>考勤详情：暂无打卡记录</p>
+            </el-space>
+          </el-card>
+        </el-timeline-item>
+        <el-timeline-item timestamp="2018/4/12" placement="top">
+          <el-card>
+            <el-space>
+              <h4>旷工</h4>
+              <p>考勤详情：暂无打卡记录</p>
+            </el-space>
+          </el-card>
+        </el-timeline-item>
+      </el-timeline>
+    </el-col>
+    <el-col :span="12">
+      <el-empty v-if="false" description="暂无申请审批" />
+      <el-timeline v-else>
+        <el-timeline-item timestamp="事假" placement="top">
+          <el-card>
+            <h4>已通过</h4>
+            <p class="apply-info">申请日期 2026-02-01 12:00:00 - 2026-02-08 12:00:00</p>
+            <p class="apply-info">申请详情 aaa</p>
+          </el-card>
+        </el-timeline-item>
+        <el-timeline-item timestamp="事假" placement="top">
+          <el-card>
+            <h4>已通过</h4>
+            <p class="apply-info">申请日期 2026-02-01 12:00:00 - 2026-02-08 12:00:00</p>
+            <p class="apply-info">申请详情 aaa</p>
+          </el-card>
+        </el-timeline-item>
+      </el-timeline>
+    </el-col>
+  </el-row>
 </template>
 
 <script setup lang="ts">
+import { ref } from 'vue'
+
+const date = ref(new Date());
+const year = date.value.getFullYear()
+const month = ref(date.value.getMonth() + 1)
+
 
 </script>
 
 <style scoped lang="scss">
-
+.exception-title {
+  margin: 20px;
+  display: flex;
+  justify-content: space-between;
+}
+.el-select {
+  width: 80px;
+}
+.el-timeline {
+  padding-left: 0px;
+  margin: 10px;
+}
+.apply-info {
+  margin: 10px;
+}
 </style>
