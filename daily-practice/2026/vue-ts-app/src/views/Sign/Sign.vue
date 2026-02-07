5f02194c18a1c70ba1834b06e57df48a1abe0baa
凡星 <m13631897256@163.com>
2026-02-07 08:24:02 +0800

    月份控件联动描述列表和日历

--- daily-practice/2026/vue-ts-app/src/views/Sign/Sign.vue
+++ daily-practice/2026/vue-ts-app/src/views/Sign/Sign.vue
@@ -1,17 +1,12 @@
 <template>
   <el-descriptions border direction="vertical" :column="9">
     <el-descriptions-item label="月份">{{ month }}月</el-descriptions-item>
-    <el-descriptions-item label="正常出勤">0</el-descriptions-item>
-    <el-descriptions-item label="旷工">0</el-descriptions-item>
-    <el-descriptions-item label="漏打卡">0</el-descriptions-item>
-    <el-descriptions-item label="迟到">0</el-descriptions-item>
-    <el-descriptions-item label="早退">0</el-descriptions-item>
-    <el-descriptions-item label="迟到并早退">0</el-descriptions-item>
+    <el-descriptions-item v-for="value, key in DetailKey" :key="key" :label="value">{{ detailValue[key] }}</el-descriptions-item>
     <el-descriptions-item label="操作">
-      <el-button type="primary" plain size="small">查看详情</el-button>
+      <el-button type="primary" plain size="small" @click="handleToException">查看详情</el-button>
     </el-descriptions-item>
     <el-descriptions-item label="考勤状态">
-      <el-tag type="success" size="small">正常</el-tag>
+      <el-tag :type="detailState.type" size="small">{{ detailState.text }}</el-tag>
     </el-descriptions-item>
   </el-descriptions>
   <el-calendar v-model="date">
@@ -19,7 +14,7 @@
       <el-button type="primary">在线签到</el-button>
       <el-space>
         <el-button plain>{{ year }}年</el-button>
-        <el-select v-model="month">
+        <el-select v-model="month" @change="handleChange">
           <el-option v-for="item in 12" :key="item" :value="item" :label="item + '月'" />
         </el-select>
       </el-space>
@@ -28,12 +23,48 @@
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue';
+import { reactive, ref } from 'vue';
+import { useRouter } from 'vue-router';
+const router = useRouter();
 
 const date = ref(new Date());
 const year = date.value.getFullYear()
 const month = ref(date.value.getMonth() + 1)
 
+enum DetailKey {
+  normal = '正常出勤',
+  absent = '旷工',
+  miss = '漏打卡',
+  late = '迟到',
+  early = '早退',
+  lateAndEarly = '迟到并早退',
+}
+
+const detailValue = reactive({
+  normal: 0,
+  absent: 0,
+  miss: 0,
+  late: 0,
+  early: 0,
+  lateAndEarly: 0,
+})
+
+const detailState = reactive({
+  type: 'success' as 'success' | 'danger',
+  text: '正常' as '正常' | '异常',
+})
+
+const handleChange = () => {
+  date.value = new Date(`${year}.${month.value}`)
+}
+const handleToException = () => {
+  router.push({
+    path: '/exception',
+    query: {
+      month: month.value
+    }
+  })
+}
 </script>
 
 <style scoped lang="scss">
